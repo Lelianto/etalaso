96a0cbcfb66a0662989d207ef2bc2f7e35e71a7d
Lelianto <lelianto.eko@gmail.com>
2026-05-02 05:46:29 +0700

    resolve

--- src/components/storefronts/StorefrontFactory.tsx
+++ src/components/storefronts/StorefrontFactory.tsx
@@ -5,11 +5,13 @@ import ClassicStorefront from './ClassicStorefront'
 import ModernStorefront from './ModernStorefront'
 import CompactStorefront from './CompactStorefront'
 import { BusinessData } from '../templates/types'
+import { TemplateFactory } from '../templates/TemplateFactory'
 
 // For now we'll define a few variants. 
 // In the future, these can be completely different components.
 const STOREFRONT_REGISTRY: Record<string, React.FC<{ business: any }>> = {
   classic: ClassicStorefront,
+  kuliner: ClassicStorefront,
   modern: ModernStorefront,
   compact: CompactStorefront,
 }
@@ -20,7 +22,11 @@ interface StorefrontFactoryProps {
 }
 
 export function StorefrontFactory({ variant = 'classic', business }: StorefrontFactoryProps) {
-  const Storefront = STOREFRONT_REGISTRY[variant] || STOREFRONT_REGISTRY.classic
+  const Storefront = STOREFRONT_REGISTRY[variant]
   
-  return <Storefront business={business} />
+  if (Storefront) {
+    return <Storefront business={business} />
+  }
+
+  return <TemplateFactory templateId={variant} business={business} orderingActive={true} />
 }
